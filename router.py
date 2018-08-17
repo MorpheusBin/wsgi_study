cde1b2e21571f614817d2a4dfeabcb22caedc446
morpheusbin <378637599@qq.com>
2018-08-17 23:36:49 +0800

    PEP8

--- router.py
+++ router.py
@@ -4,22 +4,20 @@ import wsgi
 class ControllerTest(object):
     def __init__(self):
         print "ControllerTest!!!!"
-    def test(self,req):
-          print "req",req
-          return {
+
+    def test(self, req):
+        print "req", req
+        return {
             'name': "test",
             'properties': "test"
         }
 
 
 class MyRouterApp(wsgi.Router):
-      '''
-      app
-      '''
-      def __init__(self,mapper):
-          controller = ControllerTest()
-          mapper.connect('/test',
+    def __init__(self, mapper):
+        controller = ControllerTest()
+        mapper.connect('/test',
                        controller=wsgi.Resource(controller),
                        action='test',
                        conditions={'method': ['GET']})
-          super(MyRouterApp, self).__init__(mapper)
+        super(MyRouterApp, self).__init__(mapper)
